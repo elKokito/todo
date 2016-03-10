6b9506f55d8a4f300abe00882315024407a99a79
Ubuntu <ubuntu@ip-172-31-20-250.us-west-2.compute.internal>
2016-03-10 18:04:03 +0000

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,8 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                loader: "style-loader!css-loader"}
+                loader: "style-loader!css-loader"
+            },
             {
                 test: /\.json$/,
                 loader: 'raw-loader'
